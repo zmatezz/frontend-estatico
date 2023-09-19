4c424fc5004af502648369f48e731f158afca81b
Matheus - Aula 1 <matheusguedescosta@gmail.com>
2023-09-18 21:24:49 -0300

    feat: finish aula 2 project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content:  ["./*.{html,js}"],
+  content: ["./**/*.html", "./assets/js/*.js"],
   theme: {
     extend: {
       fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
-        greatVibes: ['Great Vibes', 'cursive'],
+        poppins: ["Poppins", "sans-serif"],
+        greatVibes: ["Great Vibes", "cursive"],
       },
 
       backgroundImage: {
-        'hero': "url('https://res.cloudinary.com/dgeeyohmv/image/upload/v1693963744/fs6fghc2bzwoltyxetnb.jpg')",
-        'slider': "url('https://res.cloudinary.com/dgeeyohmv/image/upload/v1693963847/fuvme37nrpeqcsnbsnnc.jpg')",
+        hero: "url('https://res.cloudinary.com/dgeeyohmv/image/upload/v1693963744/fs6fghc2bzwoltyxetnb.jpg')",
+        slider:
+          "url('https://res.cloudinary.com/dgeeyohmv/image/upload/v1693963847/fuvme37nrpeqcsnbsnnc.jpg')",
       },
 
       button: {
-        'outline': '',
-        'solid': 'none',
-      }
-
-      
+        outline: "",
+        solid: "none",
+      },
     },
   },
   plugins: [],
-}
-
+};
